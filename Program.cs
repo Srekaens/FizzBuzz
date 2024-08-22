da4d620994ffe62c5b3828be3333b1c9243755ef
Justin Didelot <163373490+Srekaens@users.noreply.github.com>
2024-08-22 11:49:40 +0200

    Ajout du program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 0; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write($"{i} :");
+        Console.Write(fizz);
+        Console.WriteLine(buzz);
+    }
+    else if (i % 3 == 0)
+    {
+        Console.Write($"{i} :");
+        Console.WriteLine(fizz);
+    }
+    else if (i % 5 == 0)
+    {
+        Console.Write($"{i} :");
+        Console.WriteLine(buzz);
+    }
+    else
+    {
+        Console.WriteLine($"{i} : Ce nombre n'est pas divisible ni par 3 ni par 5");
+    }
+
+}
